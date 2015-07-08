a104cca11437836817c710a77807f62a341a5250
Ggoggo <cfr2ak@gmail.com>
2015-07-08 20:11:50 +0900

    frontpage structure

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ app.use( express.static( __dirname + "/public" ));
 
 app.route("/")
 	.get(function(req, res){
-		res.render("frontpage", { title : "john" } );
+		res.render("frontpage.ejs", { groups: [{name:"세상에 엿을 주자", contents:"히하"}, {name:"그냥 엿", contents:"하.."}]});
 	});
 
 app.route(/\/group\/.*/)
 	.get(function(req, res){
 		// get group info
+		res.render(".ejs", { title : "john", groups: [{name:"fuck the world", contents:"haha"}, {name:"just fuck", contents:"ha.."}]});
 	})
 	.post(function(req, res){
 		// create group
