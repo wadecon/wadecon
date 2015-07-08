273a77f5d62c49404dd21a00d3055cf34df94886
Jiwon Choi <1890mah@gmail.com>
2015-07-08 22:45:35 +0900

    오타픽스

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /// <reference path="typings/node/node.d.ts"/>
 var express = require("express");
 var app = express();
+var path = require('path');
+require('colors');
 
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/app/views");
@@ -21,3 +23,4 @@ app.route(/\/group\/.*/)
 	});
 
 app.listen(3000);
+console.log("3000번 포트에서 서버 시작".green);
