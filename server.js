8a7a21ebe7fa76ea9be9e27fa65b94e4084942ca
Ggoggo <cfr2ak@gmail.com>
2015-07-09 08:25:32 +0900

    init database

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 /// <reference path="typings/node/node.d.ts"/>
 var express = require("express");
 var app = express();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/app/views");
 app.use( express.static( __dirname + "/public" ));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var connection = mysql.createConnection({
+    host :'localhost',
+    port : 3306,
+    user : 'root',
+    password : '321654',
+    database:'wadecon'
+});
+
+connection.connect(function(err){
+	if (err) {
+	    console.error('error connecting: ' + err.stack);
+	    return;
+  	}
+ 	console.log('connected as id ' + connection.threadId);
+});
 
 app.route("/")
 	.get(function(req, res){
-		res.render("frontpage.ejs", { groups: [{name:"세상에 엿을 주자", contents:"히하"}, {name:"그냥 엿", contents:"하.."}]});
+		var query = connection.query("select * from groups",function(err, rows){
+			if (err) {
+				console.error(err);
+				throw err;
+			}
+			console.log(query);
+			res.render( "frontpage.ejs", {groups: rows} );
+		});
+	})
+	.post(function(req, res){
+		var data = {
+			group_name : req.body.group_name,
+			purpose : req.body.purpose
+		};
+		var query = connection.query("insert into groups SET ?", data,function(err,result){
+	        if (err) {
+	            console.error(err);
+	            throw err;
+	        }
+	        console.log(query);
+			res.redirect("/");
+	    });
 	});
 
 app.route(/\/group\/.*/)
