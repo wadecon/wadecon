ecc98f21bcd1f9a03ec488012d167da565b0b44a
Jiwon Choi <1890mah@gmail.com>
2015-07-09 21:03:08 +0900

    database.js에 모든 DB관련 내용 넣어서 분리, 기존 DB접근을 ORM(Sequelize.js)통한 접근으로 대체, setting.json에 DB정보 넣고 .gitignore로 숨김

--- server.js
+++ server.js
@@ -1,67 +1,58 @@
 /// <reference path="typings/node/node.d.ts"/>
 var express = require("express");
 var app = express();
-var mysql = require("mysql");
+
 var bodyParser = require("body-parser");
 var path = require('path');
+
+
+
 require('colors');
+
+set = require('./setting.json');
+require('./database.js')(this);
 
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/app/views");
 app.use( express.static( __dirname + "/public" ));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var connection = mysql.createConnection({
-    host :'localhost',
-    port : 3306,
-    user : 'root',
-    password : '321654',
-    database:'wadecon'
-});
-
-connection.connect(function(err){
-	if (err) {
-	    console.error('error connecting: ' + err.stack);
-	    return;
-  	}
- 	console.log('connected as id ' + connection.threadId);
-});
-
 app.route("/")
 	.get(function(req, res){
-		connection.query("select * from groups",function(err, rows){
-			if (err) {
-				console.error(err);
-				throw err;
-			}
-			res.render( "frontpage.ejs", {groups: rows} );
-		});
+        Works.findAll().then(function(works, err) {
+            if(err) console.error(err);
+            else {
+                res.render("frontpage.ejs", {groups: works});
+            }
+        });
 	})
 	.post(function(req, res){
 		var data = {
 			group_name : req.body.group_name,
 			purpose : req.body.purpose
 		};
-		connection.query("insert into groups SET ?", data,function(err, rows){
-	        if (err) {
-	            console.error(err);
-	            throw err;
-	        }
-			res.redirect("/");
-	    });
+		Works.create({
+			name: req.body.group_name,
+			desc: req.body.purpose
+		}).then(function(work, err) {
+			if(err) console.error(err);
+            else {
+                res.redirect('/');
+            }
+		});
 	});
 
 app.route(/\/group\/.*/)
 	.get(function(req, res){
 		var group_name = req.path.split("/").slice(-1)[0];
-		connection.query("select * from groups where group_name='"+group_name+"' limit 1",function(err, rows){
-	        if (err) {
-	            console.error(err);
-	            throw err;
-	        }
-			console.log(rows);
-			res.render("grouppage.ejs", {groups: rows} );
-	    });
+		// connection.query("select * from groups where group_name='"+group_name+"' limit 1",function(err, rows){
+	    //     if (err) {
+	    //         console.error(err);
+	    //         throw err;
+	    //     }
+		// 	console.log(rows);
+		// 	res.render("grouppage.ejs", {groups: rows} );
+	    // });
 		
 	})
 	.post(function(req, res){
