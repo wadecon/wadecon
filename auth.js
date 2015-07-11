ba3070b27ea777e9a8b21648d5291de0f4b3a82e
Jiwon Choi <1890mah@gmail.com>
2015-07-11 09:51:33 +0900

    patch

--- auth.js
+++ auth.js
@@ -26,7 +26,7 @@ function getPassport(){
 	passport.use(new FacebookStrategy({
 	        clientID: '1610016475924040',
 	        clientSecret: set.fb.secret,
-	        callbackURL: "http://"+set.host+":"+set.port+"/auth/fb/callback"
+	        callbackURL: ("http://"+set.host+((set.main)?':'+set.port:'')+"/auth/fb/callback")
 	    },
 	    function(accessToken, refreshToken, profile, done) {
 			console.log("P",profile)
