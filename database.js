21e9a6f113bb52c12528df12320952314f8d25d9
Jiwon Choi <1890mah@gmail.com>
2015-07-13 12:30:15 +0900

    공작에 시작-끝 기간(시간) 저장

--- database.js
+++ database.js
@@ -8,7 +8,8 @@ module.exports = function(global) {
         LONG_CHAR   : Sqlize.STRING(256),
         URL         : Sqlize.STRING(64),
         INT32       : Sqlize.INTEGER(32),
-        BOOLEAN     : Sqlize.BOOLEAN()
+        BOOLEAN     : Sqlize.BOOLEAN,
+        DATE        : Sqlize.DATE
     };  // domain macros
     
     console.log("DB 접속 정보 : ".cyan, set.db.database, set.db.user, set.db.password);
@@ -83,6 +84,14 @@ module.exports = function(global) {
         },
         frontboard: {
             type: DOMAIN.URL, //path
+            allowNull: true
+        },
+        startTime: {
+            type: DOMAIN.DATE,
+            allowNull: true
+        },
+        endTime: {
+            type: DOMAIN.DATE,
             allowNull: true
         }
     });
